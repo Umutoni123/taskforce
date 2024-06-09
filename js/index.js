1144d3a3c9a532ef3790cdac6dcd72ceb9bde5bc
Umutoni123 <guinkluolan@gmail.com>
2024-06-09 21:49:20 +0200

    add color and arrow

--- js/index.js
+++ js/index.js
@@ -1,51 +1,51 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     const imgContainer = document.querySelector('.image-container');
 
     const images = [
         {
             src: '../images/fox.jpg',
-            type: 'Fenec',
-            name: 'Fox',
+            type: 'FENNEC',
+            name: 'FOX',
             country: 'India'
-
         },
         {
             src: '../images/whale.jpg',
-            type: 'Humpback',
-            name: 'whale',
+            type: 'HUMPACK',
+            name: 'WHALE',
             country: 'South Africa'
-
         },
         {
             src: '../images/baboon.jpg',
-            type: 'Common Brown',
-            name: 'Baboon',
+            type: 'COMMON BROWN',
+            name: 'BABBONN',
             country: 'South Africa'
-
         },
         {
             src: '../images/deer.jpg',
-            type: 'Spotted',
-            name: 'Deer',
+            type: 'SPOTTED',
+            name: 'DEER',
             country: 'Amazon'
-
         }
+    ];
 
-    ]
     images.forEach(image => {
-        const imageItem = document.createElement('div')
-        imageItem.className = 'image-item'
-        img = document.createElement('img')
-        img.src = image.src
-        const imageDescription = document.createElement('div')
-        imageDescription.className=('des')
-        
-        imageDescription.innerHTML = `<h2 class="type">${image.type}</h2><h2 class="name">${image.name}</h2><p>${image.country}</p><div class="Read more">Read more <img src="" alt=""></div>`
+        const imageItem = document.createElement('div');
+        imageItem.className = 'image-item';
 
+        const img = document.createElement('img');
+        img.src = image.src;
 
-        
-        imageItem.appendChild(imageDescription)
-        imageItem.appendChild(img)
-        imgContainer.appendChild(imageItem)
-    })
-})
+        const imageDescription = document.createElement('div');
+        imageDescription.className = 'des';
+        imageDescription.innerHTML = `
+            <h2 class="type">${image.type} <br/> ${image.name}</h2>
+            
+            <p class="country">${image.country}</p>
+            <div class="read-more">Know more <i class="fas fa-arrow-right"></i> <img src="" alt=""></div>
+        `;
+
+        imageItem.appendChild(img);
+        imageItem.appendChild(imageDescription);
+        imgContainer.appendChild(imageItem);
+    });
+});
